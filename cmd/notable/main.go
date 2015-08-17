751ce716e4fed4b891417de1167eb48f8a941263
Jason Dew <jason.dew@gmail.com>
2015-08-17 09:51:05 -0400

    Use port number given by PORT

--- cmd/notable/main.go
+++ cmd/notable/main.go
@@ -3,10 +3,18 @@ package main
 import notable "github.com/harvesthq/notable"
 
 import (
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	http.HandleFunc("/", notable.Handler)
-	http.ListenAndServe(":5000", nil)
+	http.ListenAndServe(":"+port, nil)
 }
